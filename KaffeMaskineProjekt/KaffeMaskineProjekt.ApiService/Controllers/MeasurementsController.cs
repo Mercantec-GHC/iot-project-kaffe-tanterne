8dfb8437a69fe5d6018ef2a5a4389798401fb193
Mape0009 <mape0009@edu.mercantec.dk>
2025-04-24 10:05:00 +0200

    Simple CRUD methods for MeasurementsController complete

--- KaffeMaskineProjekt/KaffeMaskineProjekt.ApiService/Controllers/MeasurementsController.cs
+++ KaffeMaskineProjekt/KaffeMaskineProjekt.ApiService/Controllers/MeasurementsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using KaffeMaskineProject.Repository;
+using KaffeMaskineProject.DomainModels;
 
 namespace KaffeMaskineProjekt.ApiService.Controllers
 {
@@ -20,6 +21,53 @@ namespace KaffeMaskineProjekt.ApiService.Controllers
         public async Task<IActionResult> Index()
         {
             return Ok(await _context.Measurements.ToListAsync());
+        }
+
+        // GET details
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var measurements = await _context.Measurements
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (measurements == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(measurements);
+        }
+
+        // POST: Measurements
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]Measurements measurements)
+        {
+            _context.Measurements.Add(measurements);
+            _context.SaveChanges();
+            return Ok(measurements);
+        }
+
+        // PUT: Measurements
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody]Measurements measurements)
+        {
+            _context.Measurements.Update(measurements);
+            _context.SaveChanges();
+            return Ok(measurements);
+        }
+
+        // DELETE: Measurements
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var measurements = await _context.Measurements.FindAsync(id);
+            _context.Measurements.Remove(measurements);
+            _context.SaveChanges();
+            return Ok(measurements);
         }
     }
 }
