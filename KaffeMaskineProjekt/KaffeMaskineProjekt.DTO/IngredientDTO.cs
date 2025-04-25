6e4ff1d19c45dbc08bc12c9a531da06e9ab71d14
Rasmus Tanggaard <rasmus@tthost.dk>
2025-04-25 11:57:59 +0200

    Add DTO projekt

---
+++ KaffeMaskineProjekt/KaffeMaskineProjekt.DTO/IngredientDTO.cs
@@ -0,0 +1,35 @@
+﻿namespace KaffeMaskineProjekt.DTO
+{
+    public class IngredientDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// Model for creating a new ingredient
+    /// </summary>
+    public class CreateIngredientModel
+    {
+        /// <summary>
+        /// The name of the ingredient
+        /// </summary>
+        public required string Name { get; set; }
+    }
+
+    /// <summary>
+    /// Model for updating an existing ingredient
+    /// </summary>
+    public class EditIngredientModel
+    {
+        /// <summary>
+        /// The unique identifier of the ingredient
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The updated name of the ingredient
+        /// </summary>
+        public required string Name { get; set; }
+    }
+}
