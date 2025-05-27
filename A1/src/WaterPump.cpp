5d45bc9f495add689d87ce9b31d4e08dc8594335
Mape0009 <mape0009@edu.mercantec.dk>
2025-05-27 18:26:30 +0200

    Timed water pump

--- A1/src/WaterPump.cpp
+++ A1/src/WaterPump.cpp
@@ -6,20 +6,37 @@ float temp;
 int motor1pin1 = 2;
 int motor1pin2 = 3;
 
+unsigned long waterPumpStartTime = 0;
+unsigned long waterPumpStopTime = 0;
+
+bool waterPumpRunning = false;
+
+const unsigned long waterPumpRunDuration = 20000;
+
 void WaterPumpSetup() {
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
 }
 
 void WaterPumpLoop() {
+  unsigned long currentMillis = millis();
   temp = ds.getTempC();
   Serial.print("Temperature: ");
   Serial.println(temp);
-  delay(1000);
 
   if (temp > 30.0) {
-    digitalWrite(motor1pin1, HIGH);
+    if (!waterPumpRunning) {
+      digitalWrite(motor1pin1, HIGH);
+      waterPumpStartTime = currentMillis;
+      waterPumpRunning = true;
+    } else {
+      if (currentMillis - waterPumpStartTime >= waterPumpRunDuration) {
+        digitalWrite(motor1pin1, LOW);
+        waterPumpRunning = false;
+      }
+    }
   } else {
     digitalWrite(motor1pin1, LOW);
+    waterPumpRunning = false;
   }
 }
