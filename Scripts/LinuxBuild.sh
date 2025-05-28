9957ba87d5853112d92a78656d9cbd1bf90fb8dc
Rasmus Tanggaard <rasmus@tthost.dk>
2025-05-28 11:26:26 +0200

    Update LinuxBuild.sh

--- Scripts/LinuxBuild.sh
+++ Scripts/LinuxBuild.sh
@@ -1,3 +1,6 @@
+# git pull with submodules before everything
+(cd ../ && git pull --recurse-submodules)
+
 # Remove package-lock.json if it exists in AppHost
 if [ -f "../KaffeMaskineProjekt.AppHost/package-lock.json" ]; then
     rm ../KaffeMaskineProjekt.AppHost/package-lock.json
@@ -11,5 +14,5 @@ fi
 # Run aspire publish -o publish in KaffeMaskineProjekt
 (cd ../ && aspire publish -o publish)
 
-# Run docker compose up in the publish folder
-(cd ../publish && docker compose up)
+# Run docker compose up in the publish folder with KAFFEDBSERVER_PASSWORD env variable
+(cd ../publish && KAFFEDBSERVER_PASSWORD=callmeal docker compose up)
